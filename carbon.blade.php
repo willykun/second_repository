9a4939046b6a4942e231788da676cd3235f19583
willykun <wdaniel.20.20@gmail.com>
2015-08-10 15:11:41 -0430

    segundo commit

---
+++ carbon.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Fechas</div>
+
+				<div class="panel-body">
+					 esta es la fecha {{$date}} </br>
+					
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
